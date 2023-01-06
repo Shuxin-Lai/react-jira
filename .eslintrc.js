cb85dd875e0d8a84b09f8260d7600a19b57e4e09
Shuxin-Lai <shuxinlai001@gmail.com>
2023-01-06 23:26:01 +0800

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,9 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint"],
-  rules: {},
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
   settings: {
     react: {
       version: "detect",
